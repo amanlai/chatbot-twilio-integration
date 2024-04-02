1b6e9042abf271114155f89051c9e3d400e8d0ad
amanlai <manlai.lp@gmail.com>
2024-04-02 15:57:15 -0700

    connect a single db collection with an app

--- app.py
+++ app.py
@@ -1,7 +1,5 @@
 import os
-import sys
-from re import sub, search
-from pickle import dumps, loads
+from re import sub
 from dotenv import load_dotenv
 from langchain_core.messages import AIMessage, HumanMessage
 from chainlit.server import app
@@ -12,49 +10,13 @@ from twilio.http.async_http_client import AsyncTwilioHttpClient
 from utils.helpers import init_connection, get_agent_executor
 load_dotenv()
 
-port = int(os.getenv("PORT", 8000))
+# port = int(os.getenv("PORT", 8000))
 database_name = os.getenv("DB_NAME", "chattabot")
-
-# twilio config
+business_name = os.getenv("BUSINESS_NAME")
+chat_history_db = os.getenv("CHAT_HISTORY_DB", "chat_history")
 account_sid = os.getenv("TWILIO_ACCOUNT_SID")
 auth_token = os.getenv("TWILIO_AUTH_TOKEN")
 from_phone = os.getenv("TWILIO_PHONE")
-
-# pattern to identify business name
-pattern = r"I want to ask about ([\w\_\-\s]+)\.*"
-
-
-# initialize variables
-async def startup(business_name, to_phone_number=None):
-    """
-    Given a business name 
-      - Make connection to MongoDB
-      - Get relevant documents
-      - Construct langchain agent executor
-
-    Returns agent executor
-    """
-    agent_executor = None
-    # initiate mongodb connection
-    conn = await init_connection()
-    # index into database
-    db = conn[database_name]
-    # only continue if the collection already exists in db
-    if business_name in db.list_collection_names():
-        # get data from mongodb
-        collection = db[business_name]
-        agent_executor = await get_agent_executor(collection)
-        # initialize chat history for the phone number
-        if to_phone_number is not None:
-            db_chat_history = conn["chat_history"]
-            coll_chat_history = db_chat_history[to_phone_number]
-            coll_chat_history.delete_many({})
-            coll_chat_history.insert_one(
-                {"_id": business_name, "history": dumps([])}
-            )
-    # close db connection
-    conn.close()
-    return agent_executor
 
 
 async def create_answer(agent_executor, chat_history, question):
@@ -64,15 +26,16 @@ async def create_answer(agent_executor, chat_history, question):
       - previous chat history 
       - a new question
     """
+    ch = [
+        HumanMessage(content=x) if i % 2 == 0 else AIMessage(content=x)
+        for i, x in enumerate(chat_history)
+    ]
     result = await agent_executor.ainvoke(
-        {"input": question, "chat_history": chat_history}
+        {"input": question, "chat_history": ch}
     )
-    answer = result['output']
+    answer = result["output"]
     answer = sub("^System: ", "", sub("^\\??\n\n", "", answer))
-    chat_history.extend(
-        (HumanMessage(content=question), AIMessage(content=answer))
-    )
-    return answer, chat_history
+    return answer
 
 
 async def send_sms(message, to_phone_number):
@@ -87,47 +50,6 @@ async def send_sms(message, to_phone_number):
     return message.status, message.sid
 
 
-async def business_init(is_init, to_phone_number):
-
-    name = is_init[1]
-    name = ''.join(c for c in name.lower() if c.isalnum())
-    ag_ex = await startup(name, to_phone_number)
-    if ag_ex is None:
-        answer = "I don't know about this business. Please ask me about a business that I know about."
-    else:
-        answer = "Sure. Ask me anything!"
-    return answer
-
-
-async def answer_question(question, to_phone_number):
-
-    # pull up chat history data for the phone number from the database
-    conn = await init_connection()
-    db_chat_history = conn["chat_history"]
-    coll_db_history = db_chat_history[to_phone_number]
-    chat_history_data = next(coll_db_history.find({}))
-    business_name = chat_history_data["_id"]
-    # chat history has to be unpickled to be parsed
-    chat_history = loads(chat_history_data["history"], encoding="bytes")
-
-    # pull up the agent executor from the database
-    db = conn[database_name]
-    collection = db[business_name]
-
-    # construct agent executor
-    agent_executor = await get_agent_executor(collection)
-    answer, chat_history = await create_answer(agent_executor, chat_history, question)
-    # chat history has to be pickled to be dumped into db
-    chat_history = dumps(chat_history)
-    coll_db_history.update_one(
-        {"history": {"$exists": True}},
-        {"$set": {"history": chat_history}}
-    )
-    # close database connection
-    conn.close()
-    return answer
-
-
 @app.post("/sms")
 async def chat(request: Request):
     """Respond to incoming calls with a simple text message."""
@@ -137,17 +59,32 @@ async def chat(request: Request):
     fm = await request.form()
     to_phone_number = fm.get("From")
     question = fm.get("Body")
-    is_init = search(pattern, question)
-    if is_init:
-        answer = await business_init(is_init, to_phone_number)
-    elif question == "I am done.":
-        os.execl(sys.executable, sys.executable, *sys.argv)
-    else:
-        answer = await answer_question(question, to_phone_number)
+
+    conn = await init_connection()
+    # get agent executor
+    db = conn[database_name]
+    collection = db[business_name]
+    agent_executor = await get_agent_executor(collection)
+    # get chat history
+    db_chat_history = conn[chat_history_db]
+    coll_chat_history = db_chat_history[to_phone_number]
+    chat_history_data = next(coll_chat_history.find({}), {"history": []})
+    chat_history = chat_history_data["history"]
+    # get answers
+    answer = await create_answer(agent_executor, chat_history, question)
+
+    # send SMS back
     mstatus, msid = await send_sms(answer, to_phone_number)
+
+    # update the chat history of this phone number
+    chat_history.extend((question, answer))
+    coll_chat_history.delete_many({})
+    coll_chat_history.insert_one({"history": chat_history})
+    conn.close()
+
     print(f"Message status: {mstatus}; message SID: {msid}\n")
     return {"answer": answer, "question": question}
 
 
 if __name__ == "__main__":
-    app.run(debug=True, port=port)
+    app.run(debug=True)
